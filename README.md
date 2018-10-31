966950cc04efb6b13f993700cb2a8b705207eefc
Max <tutv95@gmail.com>
2018-10-31 15:50:18 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hello-socket.io
 A example chat app built Node.js and Socket.io
+
+
+## Getting started
+```
+# Clone project
+git clone https://github.com/spy126/hello-socket.io.git
+
+# Install dependency packages
+npm install
+
+# Run app
+npm start
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require('express')()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+  console.log('a user connected')
+  socket.broadcast.emit('hi')
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(4000, () => {
+  console.log('The server is running: http://localhost:4000')
+})
+
